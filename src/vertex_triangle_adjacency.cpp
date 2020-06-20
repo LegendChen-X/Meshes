a5fc1c3e6d0b1e03e9453cbfa064b6fa1178bb36
Xiang <chenxiang00825@gmail.com>
2020-06-20 12:28:51 -0400

    cxnb

--- src/vertex_triangle_adjacency.cpp
+++ src/vertex_triangle_adjacency.cpp
@@ -5,9 +5,10 @@ void vertex_triangle_adjacency(
   const int num_vertices,
   std::vector<std::vector<int> > & VF)
 {
-  VF.resize(num_vertices);
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
+    VF.resize(num_vertices);
+    
+    for(int i=0;i<F.rows();++i)
+        for(int j=0;j<F.cols();++j)
+            VF[F(i,j)].push_back(i);
 }
 

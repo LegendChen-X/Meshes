5696070304b0ed339985e11e96b22b6ebf2b83fb
Xiang <chenxiang00825@gmail.com>
2020-06-20 11:54:21 -0400

    cxnb

--- src/triangle_area_normal.cpp
+++ src/triangle_area_normal.cpp
@@ -6,6 +6,5 @@ Eigen::RowVector3d triangle_area_normal(
   const Eigen::RowVector3d & b, 
   const Eigen::RowVector3d & c)
 {
-    Eigen::RowVector3d norm = (0.5 * (b-a).cross(c-a).norm()) * (b-a).cross(c-a).normalized();
-    return norm;
+    return (0.5 * (b-a).cross(c-a).norm()) * (b-a).cross(c-a).normalized();
 }

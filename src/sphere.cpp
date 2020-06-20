df6db448c8aeea775f01fc0eade515c8a1fd78ec
Xiang <chenxiang00825@gmail.com>
2020-06-20 11:45:29 -0400

    sphere

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -11,7 +11,54 @@ void sphere(
   Eigen::MatrixXd & NV,
   Eigen::MatrixXi & NF)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
+    int total_f = num_faces_u * num_faces_v;
+    int total_v = (num_faces_u + 1) * (num_faces_v + 1);
+    V.resize(total_v,3);
+    F.resize(total_f,4);
+    UV.resize(total_v,2);
+    UF.resize(total_f,4);
+    NV.resize(total_v,3);
+    Nf.resize(total_f,4);
+    
+    double alpha = 2.0 * M_PI / num_faces_u;
+    double beta = M_PI / num_faces_v;
+    
+    int index = 0;
+    
+    for(int i=0;i<num_faces_u+1;++i)
+    {
+        for(int j=0;j<num_faces_v+1;++j)
+        {
+            double x = cos(alpha*i) * sin(beta*j);
+            double y = sin(alpha*i) * sin(beta*j);
+            double z = cos(beta*j);
+            
+            V.row(index) = Eigen::Vector3d(x,y,z);
+            
+            double p_x = (double) i / (double) (num_faces_u+1);
+            double p_y = (double) j / (double) (num_faces_v+1);
+            UV.row(index) = Eigen::Vector2d(p_x,p_y);
+            
+            NV.row(index) = Eigen::Vector3d(x,y,z);
+            
+            index += 1;
+        }
+    }
+    
+    index = 0;
+    for(int i=0;i<num_faces_u;++i)
+    {
+        for(int j=0;j<num_faces_v;++j)
+        {
+            int v_1 = i*(num_faces_v+1)+j;
+            int v_2 = (i+1)*(num_faces_v+1)+j;
+            int v_3 = v_2+1;
+            int v_4 = v_1+1;
+            F.row(index) = Eigen::RowVector4i(v_1,v_2,v_3,v_4);
+            UF.row(index) = Eigen::RowVector4i(v_1,v_2,v_3,v_4);
+            NF.row(index) = Eigen::RowVector4i(v_1,v_2,v_3,v_4);
+            
+            index += 1;
+        }
+    }
 }

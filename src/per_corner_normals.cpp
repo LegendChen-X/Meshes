1c4c978837b0a365fa7853121e79b77015d9d8f1
Xiang <chenxiang00825@gmail.com>
2020-06-20 23:11:33 -0400

    debug per_corner_normals

--- src/per_corner_normals.cpp
+++ src/per_corner_normals.cpp
@@ -3,6 +3,7 @@
 // Hint:
 #include "vertex_triangle_adjacency.h"
 #include <iostream>
+#define DEBUG 1
 
 void per_corner_normals(
   const Eigen::MatrixXd & V,
@@ -13,7 +14,10 @@ void per_corner_normals(
     N = Eigen::MatrixXd::Zero(F.rows()*3,3);
     std::vector<std::vector<int>> VF;
     vertex_triangle_adjacency(F,V.rows(),VF);
-    double x = cos(corner_threshold*M_PI/180.0);
+    
+#if(DEBUG)
+    printf("vertex_triangle_adjacency check\n");
+#endif
     
     int index = 0;
     for(int i=0;i<F.rows();++i)
@@ -23,16 +27,34 @@ void per_corner_normals(
             Eigen::RowVector3d alpha(0,0,0);
             double beta = 0;
             Eigen::RowVector3d buff_f = triangle_area_normal(V.row(F(i,0)),V.row(F(i,1)),V.row(F(i,2)));
-            for(int l=0;i<VF[F(i,j)].size();++l)
+            for(auto l:VF[F(i,j)])
             {
-                Eigen::RowVector3d buff_g = triangle_area_normal(V.row(F(VF[F(i,j)][l],0)),V.row(F(VF[F(i,j)][l],1)),V.row(F(VF[F(i,j)][l],2)));
-                if((buff_g.dot(buff_f) / (buff_g.norm()*buff_f.norm())) > x)
+#if(DEBUG)
+                printf("VF[F(i,j)].size(): %lu\n",VF[F(i,j)].size());
+#endif
+                
+#if(DEBUG)
+                printf("triangle_area_normal start\n");
+#endif
+                
+                Eigen::RowVector3d buff_g = triangle_area_normal(V.row(F(l,0)),V.row(F(l,1)),V.row(F(l,2)));
+                
+#if(DEBUG)
+                printf("triangle_area_normal check\n");
+#endif
+                
+#if(DEBUG)
+                printf("value: %f\n",(buff_g.dot(buff_f) / (buff_g.norm()*buff_f.norm())));
+#endif
+                
+                if((buff_g.dot(buff_f) / (buff_g.norm()*buff_f.norm())) > cos(corner_threshold*M_PI/180.0))
                 {
                     alpha += buff_g;
                     beta += buff_g.norm();
                 }
             }
             N.row(index) = (alpha/beta).normalized();
+            index++;
         }
     }
 }
